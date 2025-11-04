49ec8b63454f4eb1c1bd40dd7820437f72da5f3c
Katia <katia.punter@storyshareplatform.com>
2025-11-04 21:57:50 +0000

    adds a data view

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-import pandas as pd # Import the pandas library for handling data in tables (DataFrames)
-import streamlit as st # Import Streamlit for building the web app interface
+import pandas as pd # for handling data in DataFrames
+import streamlit as st # for building the web app interface
 
 
 st.set_page_config(page_title="Celebrity Height Comparison", page_icon="📏")
-st.title("📏 Celebrity Height Comparison")
-st.write("Hello—this is your app. It’s running.")
+
+st.title("📊 Celebrity Data Preview")
+
+celebs_df = pd.read_csv("celebs_height.csv")
+
+st.dataframe(celebs_df)
